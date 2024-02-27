cc9994c1db3cc870231847b31e564cd043219341
HiathanNguyen <m264632@usna.edu>
2024-02-27 09:10:04 -0500

    food.py

--- food.py
+++ food.py
@@ -1,7 +1,7 @@
 import pandas as pd
 csv = pd.read_csv('FF_SR_ data.csv')
 df = csv[['food_category_id','SR_Component','SR Mean per 100g','FF Mean per 100g']]
-df = df[df['SR_Component'].str.contains('SFA') == False]
+df = df[df['SR_Component'].str.contains(':') == False]
 df["PC"] = abs((df["FF Mean per 100g"]-df["SR Mean per 100g"])/(df["SR Mean per 100g"]))
 df2 = pd.read_csv('food_category_id table.csv')
 df = df.merge(df2, how='left').fillna(0)
