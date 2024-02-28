7445ee67c17a498c8f5b4dd8896f566b77e19145
Cole Regan <m265232@usna.edu>
2024-02-28 14:03:03 -0500

    Hiathan.py

--- Hiathan.py
+++ Hiathan.py
@@ -5,14 +5,15 @@ df5 = csv[['food_category_id','FF Food description','FF_Component','SR Mean per 
 df5 = df5[df5['FF_Component'].str.contains('SFA') == False]
 df6 = pd.read_csv("FF_SR_ data.csv")
 df5["PC"] = abs((df6["FF Mean per 100g"]-df6["SR Mean per 100g"])/(df6["SR Mean per 100g"])) * 100
-LEGUME = df5[df5['food_category_id'] == 16]
+LEGUME = df5[df5['food_category_id'] == 7]
 LEGUME = LEGUME.sort_values(by="PC", ascending=False)
 LEGUME = LEGUME[LEGUME['PC'] != 0]
 LEGUME = LEGUME[LEGUME['SR Mean per 100g'] != 0]
-topten = LEGUME[0:17]
+topten = LEGUME[0:11]
 
 
 
-fig = px.histogram(topten, y='PC', x='FF_Component', title='Percent Change in Top 10 Legume Nutrients',text_auto='.3s')
+fig = px.histogram(topten, y='PC', x='FF_Component', title='Percent Change in Top 10 Sausages and Luncheon Meats Nutrients',text_auto='.3s')
 fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
 fig.show()
